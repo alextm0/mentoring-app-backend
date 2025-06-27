772bf4136de806e78eba68a1b70a0d818dc4647e
Toma Alexandru <alextoma1704@gmail.com>
2025-06-27 23:12:25 +0300

    Initial commit

---
+++ src/test/java/com/mentoringapp/MentoringApplicationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mentoringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentoringApplicationBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
